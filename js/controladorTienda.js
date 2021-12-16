b8304c3bb65962de5f977314b47e717de711df24
Carlos Ospina <69280489+dany-uz@users.noreply.github.com>
2021-12-16 18:31:26 -0500

    se crea boton carrito

--- js/controladorTienda.js
+++ js/controladorTienda.js
@@ -9,6 +9,7 @@ let producto={}
 //Referencias a etiquetas del HTML
 let filaproductos=document.getElementById("fila")
 let modalInfoProducto = new bootstrap.Modal(document.getElementById('infoProducto'))
+let botonAgregarCarrito=document.getElementById("botonAgregarCarrito")
 
 //Escucho cuando hagan clic en la fila de los productos
 filaproductos.addEventListener("click",function(event){
